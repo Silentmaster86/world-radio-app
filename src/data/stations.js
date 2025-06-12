3735401303bdc5e5827f8ed1537426e752847155
Przemysław Pietkun <przemyslaw.pietkun@gmail.com>
2025-06-12 13:50:48 +0100

    added now radio logos

--- src/data/stations.js
+++ src/data/stations.js
@@ -1,52 +1,63 @@
+import capitalDance from "../assets/logos/capital_dance.png";
+import capitalFM from "../assets/logos/capital.png";
+import heartUK from "../assets/logos/heart_uk.png";
+import kissFM from "../assets/logos/kiss_fm.png";
+import radioX from "../assets/logos/radio_X.png";
+import prChicago from "../assets/logos/pr_chicago.png";
+import prl from "../assets/logos/polish_radio_london.png";
+import radioBielsko from "../assets/logos/radio_bielsko.png";
+import radioEska from "../assets/logos/radio_eska.png";
+import rmfMaxx from "../assets/logos/rmf_maxx.png";
+
 export const stations = [
     {
       name: "Capital Dance",
       url: "https://media-ice.musicradio.com/CapitalDance",
-      logo: "https://ichef.bbci.co.uk/images/ic/320x320/p0jgy4mq.jpg.webp",
+      logo: capitalDance,
     },
     {
       name: "Capital FM",
       url: "https://media-ice.musicradio.com/CapitalMP3",
-      logo: "https://seeklogo.com/images/C/capital-fm-logo-B71E94AD3E-seeklogo.com.png",
+      logo: capitalFM,
     },
     {
       name: "Heart UK",
       url: "https://media-ice.musicradio.com/HeartUK",
-      logo: "https://p1mediagroup.com/wp-content/uploads/2016/11/Heart259x259.png",
+      logo: heartUK,
     },
     {
       name: "KISS FM",
       url: "https://edge-bauerall-01-gos2.sharp-stream.com/kissnational.aac",
-      logo: "https://media.bauerradio.com/image/upload/c_crop,g_custom/v1730224745/brand_manager/stations/gu7xhzpsjio7ga82trp2.png",
+      logo: kissFM,
     },
     {
       name: "RADIOx 90s",
       url: "https://media-ice.musicradio.com/RadioX90s",
-      logo: "https://radiotoday.co.uk/wp-content/uploads/2025/02/radiox90s.jpg",
+      logo: radioX,
     },
     {
       name: "PR Chicago",
       url: "https://s1.reliastream.com/proxy/polskieradio?mp=/stream",
-      logo: "https://polskieradio.com/wp-content/themes/PRCTheme/assets/images/logo.png",
+      logo: prChicago,
     },
     {
       name: "PRL",
       url: "https://stream.rcs.revma.com/prfmwmwy768uv",
-      logo: "https://media.info/l/f/7/771.1428765955.png",
+      logo: prl,
     },
     {
       name: "Radio Bielsko",
       url: "https://stream.rcs.revma.com/062uq3z3kwzuv",
-      logo: "https://www.radiobielsko.pl/player/assets/image/rb.png",
+      logo: radioBielsko,
     },
     {
       name: "Radio Eska",
       url: "https://ic1.smcdn.pl/2330-1.mp3",
-      logo: "https://streamurl.link/images/broadcasts/701188e.webp",
+      logo: radioEska,
     },
     {
       name: "RMF MAXX",
       url: "https://rs201-krk.rmfstream.pl/rmf_maxxx",
-      logo: "https://streamurl.link/images/broadcasts/a977cab.webp",
+      logo: rmfMaxx,
     }
   ];
